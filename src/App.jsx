a70d50f37d51f4cfa1553e42e8838915254cdf7f
kingdeepanshu <king.deepanshu24@gmail.com>
2024-07-05 22:53:53 +0530

    header1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header1 from './components/Header1';
+
+function App() {
+  return (
+    <div>
+      <Header1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header1() {
+  return (
+    <div>
+      header one 
+    </div>
+  )
+}
+
+export default Header1
